3d52ccd4135d8f5437d8e14a420d8c6696740614
Spawn9986 <scouch9986@gmail.com>
2023-03-26 13:45:33 -0500

    added App.js for App component and imported to index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-// Import the React and ReactDOM libraries
+// Import the React and ReactDOM libraries and App.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 // Get a reference to the div with ID root
 const el = document.getElementById('root');
 
 // Tell React to take control of root
 const root = ReactDOM.createRoot(el);
-
-// Create a component
-function App() {
-    return <h1>Hi there!</h1>
-}
 
 // Render component on the screen
 root.render(<App />);
